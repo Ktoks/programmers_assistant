bd8d2679af8cf8488979f3b00c939c17934a8257
Jack Saller <jrsaller@gmail.com>
2019-11-08 10:07:11 -0700

    analyzeSentence and early voiceRecognition

--- Saber.py
+++ Saber.py
@@ -1,13 +1,92 @@
+import snowboydecoder
+import sys
+import signal
+
+interrupted = False
+
+def signal_handler(signal, frame):
+    global interrupted
+    interrupted = True
+
+def interrupt_callback():
+    global interrupted
+    return interrupted
+
+if len(sys.argv) == 1:
+    print("Error: need to specify model name")
+    print("Usage: python demo.py your.model")
+    sys.exit(-1)
+
+model = sys.argv[1]
+
+signal.signal(signal.SIGINT, signal_handler)
+
+detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
+print('Listening... Press Ctrl+C to exit')
+
+detector.start(detected_callback=snowboydecoder.ding_callback,
+               interrupt_check=interrupt_callback,
+               sleep_time=0.03)
+
+detector.terminate(),
+
+
 class SABER:
     
     def __init__(self):
         mLocationMap = {word:folder}
         mConfig = {setting:action}
         mLocation = USA
-        mLanguage = Eng
+        mLanguage = Eng    
+    
+
+#### HELPER FUNCTIONS FOR LISTEN ####
+
+    def signal_handler(signal, frame):
+        global interrupted
+        interrupted = True
+
+    def interrupt_callback():
+        global interrupted
+        return interrupted
+
+######  END HELPER FUNCTIONS  #########
+
+    def listen(self):
+	if len(sys.argv) == 1:
+    		print("Error: need to specify model name")
+    		print("Usage: python demo.py your.model")
+    		sys.exit(-1)
+
+	model = sys.argv[1]
+
+	signal.signal(signal.SIGINT, signal_handler)
+
+	detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
+	print('Listening... Press Ctrl+C to exit')
+
+	detector.start(detected_callback=snowboydecoder.ding_callback,
+               		 interrupt_check=interrupt_callback,
+               		      sleep_time=0.03)
+
+	detector.terminate()
+
+
 
 # def wait():
 #     msvcrt.getch()
+
+def analyzeSentence(sentence):
+	args = sentence.split()
+	argumentDictionary = {}
+	argumentDictionary["directory"] = findDirectory(args[1])
+	argumentDictionary["language"] = findLanguage(arg[2])
+	argumentDictionary["command"] = matchCommands(args[3:])
+	return argumentDictionary
+
+def voiceRecognition():
+	saber = SABER()
+	saber.listen()
 
 def main():
     saber = SABER()
