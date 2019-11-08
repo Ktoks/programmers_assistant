80922e32f98884f77c31813ae737948b706ce6de
kacystocks <kacystocks0811@gmail.com>
2019-11-08 12:03:45 -0700

    reformatted and corrected minor errors

--- Saber.py
+++ Saber.py
@@ -1,58 +1,61 @@
 import snowboydecoder
 import sys
 import signal
+import showDisplayAndMatchCommands
 
+INTERRUPTED = False
 class SABER:
 
     def __init__(self):
-        mLocationMap = {word:folder}
-        mConfig = {setting:action}
-        mLocation = USA
-        mLanguage = Eng    
-    
+        mLocationMap = {}   # word:folder
+        mConfig = {}        # setting:action
+        mLocation = {}      # location:directory
+        mLanguage = {}      # language:configuration
+
 
 #######     HELPER FUNCTIONS FOR LISTEN      ########
 
-    def signal_handler(signal, frame):
-        global interrupted
-        interrupted = True
+    def signal_handler(self, signal, frame):
+        global INTERRUPTED
+        INTERRUPTED = True
 
-    def interrupt_callback():
-        global interrupted
-        return interrupted
+    def interrupt_callback(self):
+        global INTERRUPTED
+        return INTERRUPTED
 
 ######      END HELPER FUNCTIONS      ###############
 
-    def findDirectory(arg):
-        for key in mLocationMap:
+    def findDirectory(self, arg):
+        for key in self.mLocation:
             if arg == key:
-                return mLocationMap[key]
+                return self.mLocation[key]
         return "Directory not found."
 
     def listen(self):
-	if len(sys.argv) == 1:
-    		print("Error: need to specify model name")
-    		print("Usage: python demo.py your.model")
-    		sys.exit(-1)
+        if len(sys.argv) == 1:
+            print("Error: need to specify model name")
+            print("Usage: python demo.py your.model")
+            sys.exit(-1)
 
-	model = sys.argv[1]
+        model = sys.argv[1]
 
-	signal.signal(signal.SIGINT, signal_handler)
+        signal.signal(signal.SIGINT, signal_handler)
 
-	detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
-	print('Listening... Press Ctrl+C to exit')
+        detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
+        print('Listening... Press Ctrl+C to exit')
 
-	detector.start(detected_callback=snowboydecoder.ding_callback,
-               		 interrupt_check=interrupt_callback,
-               		      sleep_time=0.03)
+        detector.start(detected_callback=snowboydecoder.ding_callback,
+                         interrupt_check=interrupt_callback,
+                              sleep_time=0.03)
 
-	detector.terminate()
+        detector.terminate()
 
-        mLocation = args[1]
-        mLanguage = args[2]
-        mCommand = args[3]
+        mLocation = model[1]
+        mLanguage = model[2]
+        mCommand = model[3]
 
-    def findDirectory(location):
+    def findDirectory(self, location):
+        pass
         # NEEDS TO LOOKUP THE COMMAND AND COMPARE location WITH A SAVED LOCATION IN OUR DICTIONARY
         # return the actual location
 
@@ -60,15 +63,32 @@ class SABER:
 # def wait():
 #     msvcrt.getch()
 
-def analyzeSentence(sentence):
-    	args = sentence.split()
-	argumentDictionary = {}
-	argumentDictionary["directory"] = findDirectory(args[1])
-	argumentDictionary["language"] = findLanguage(arg[2])
-	argumentDictionary["command"] = matchCommands(args[3:])
-	return argumentDictionary
+# def analyzeSentence(self, sentence):
+#     args = sentence.split()
+#     argumentDictionary = {}
+#     argumentDictionary["directory"] = findDirectory(args[1])
+#     argumentDictionary["language"] = findLanguage(arg[2])
+#     argumentDictionary[
+#         "command"] = showDisplayAndMatchCommands.matchCommands(args[3:])
+#     return argumentDictionary
 
-def voiceRecognition():
-	saber = SABER()
-	saber.listen()
+    def voiceRecognition(self):
+        saber = SABER()
+        saber.listen()
 
+    def analyzeSentence(self, sentence):
+        args = sentence.split('python')
+        if len(args) == 1:
+            print("Must include python in string")
+            return
+        directoryList = args[0].split()
+        actualDirectory = directoryList[1:].join(" ")
+        argumentDictionary = {}
+        argumentDictionary["directory"] = findDirectory(actualDirectory)
+        #argumentDictionary["language"] = findLanguage(arg[2])
+        argumentDictionary[
+            "command"] = showDisplayAndMatchCommands.matchCommands(args[1])
+        return argumentDictionary
+
+# def voiceRecognition():
+# 	return saber.listen()
--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-import saber
+import Saber
+import sys
+import getopt
 # def wait():
 #     msvcrt.getch()
 
-def analyzeSentence(sentence):
-	args = sentence.split('python')
-	if len(args) == 1:
-		print("Must include python in string")
-		return
-	directoryList = args[0].split()
-	actualDirectory = directoryList[1:].join(" ")
-	argumentDictionary = {}
-	argumentDictionary["directory"] = findDirectory(actualDirectory)
-	#argumentDictionary["language"] = findLanguage(arg[2])
-	argumentDictionary["command"] = matchCommands(args[1])
-	return argumentDictionary
-
-def voiceRecognition():
-	return saber.listen()
-
-def main():
+def main(argv):
     try:
         opts, args = getopt.getopt(argv, opts)
     except getopt.GetoptError:
@@ -27,16 +13,16 @@ def main():
     saber = saber.SABER()
     #voiceRecognition(saber)?????
     keyWord = input('Listening... Press Ctrl+C to exit')
-    if keyword == 'start':
+    if keyWord == 'start':
         running = True
         while running == True:
             keyWord = input('Listening... Press Ctrl+C to exit')
-            sentence = voiceRecognition(keyword)
-            analyzeSentence(sentence)
-            location = findDirectory[arg1]
-            display.showDisplay()
+            sentence = saber.voiceRecognition(keyWord)
+            saber.analyzeSentence(sentence)
+            location = saber.findDirectory[args[1]]
+            saber.display.showDisplay()
             keyWord = input('Listening... Press Ctrl+C to exit')
-            if keyword == 'quit':
+            if keyWord == 'quit':
                 running = False
 
 main()
