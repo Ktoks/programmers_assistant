016c53c950d41199eeeef0de5c35b7d072e7a9ff
DamenMaughan <D00351373@dmail.dixie.edu>
2019-11-08 13:23:16 -0700

    aa

--- tools.py
+++ tools.py
@@ -1,33 +1,50 @@
+FORGIVENESS_ERROR = 0.1
+from tkinter import messagebox
+import tkinter as tk
 
+class InvalidCommand(Exception):
+    pass
 
-def similarity(target,options,accuracy):
+def fix_word(target,options,accuracy):
+    root = tk.Tk()
+    root.withdraw()
     #this function receives a target string and a list of optional strings it
     #   might match. It returns the highest accuracy option if it is within
     #   _accuracy_ of target. Otherwise, returns False for not recognized.
     tests = []
     for i in range(len(options)):
-        x = similarityR(target,options[i])
-        x = (2*x)/(len(target) + len(option))
-        tests.append(i,x)
+        x = fix_word_R(target,options[i])
+        x = (2*x)/(len(target) + len(options[i]))
+        tests.append([i,x])
     #check all
     best = 0
     tests = sort_col(tests,1)
+    tests.reverse()
     bests = 1
     while True:
-        if abs(tests[-(bests+1)][1] - tests[-1][1]) < 0.01 :
+        if bests < len(tests) and abs(tests[bests][1] - tests[0][1]) < 0.01 :
             bests += 1
         else:
             break
-    if bests > 1:
+    if bests > 2 and tests[-0][1] > accuracy:
+        messagebox.showwarning("Error: Vague","Command failed: '"+target+
+                              "'too vague, "+str(bests) +
+                              " possible interpretations.")
+        raise InvalidCommand
+    if bests > 1 and tests[0][1] > accuracy:
         print("There's more than 1 best option. Idk what to do with this.  Total:",bests)
-    elif tests[-1][1] > accuracy:
-        print("There's a winner:",options[tests[-1][0]])
-    elif tests[-1][1] > accuracy - FORGIVENESS_ERROR:
-        print("There's something close to being valid. Did you mean:",options[tests[-1][0]])
+    elif tests[0][1] > accuracy:
+        return options[tests[0][0]]
+    elif tests[0][1] > accuracy - FORGIVENESS_ERROR:
+        result = messagebox.askyesno("Suggestion","Did you mean: '"+options[tests[0][0]]+"'?")
+        if result:
+            return options[tests[0][0]]
+        else:
+            raise InvalidCommand
     else:
         print("Nothing is close. Try again matey.")
 
-def similarityR(target,option):
+def fix_word_R(target,option):
     #print(target,option)
     if len(target) == 0 or len(option) == 0:
         return 0
@@ -44,8 +61,8 @@ def similarityR(target,option):
     if not subs:
         return 0
     myLen = subs[0][1] - subs[0][0]
-    left = similarityR(small[:subs[0][0]],big[:subs[1][0]])
-    right = similarityR(small[subs[0][1]:],big[subs[1][1]:])
+    left = fix_word_R(small[:subs[0][0]],big[:subs[1][0]])
+    right = fix_word_R(small[subs[0][1]:],big[subs[1][1]:])
     #print(myLen,left,right)
     return myLen + left + right
 
@@ -85,11 +102,13 @@ def sort_col(data,col):
     return new
 
 t = "arrow"
-o = "arow"
+#o = ["crow","blow","asglaglvsk","agrw","arw"]
+o = ["arow","arow","arow"]
 #t ="i am going home"
 #o = "gone home"
 #t = "test"
 #o = "text"
 #t = "st"
 #o = "xt"
-print((2*similarityR(t,o))/(len(t) + len(o)))
+fix_word(t,o,0.8)
+
